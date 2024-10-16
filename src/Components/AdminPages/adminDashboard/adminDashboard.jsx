d0090005f89a1c4d04b38b095f81c08fedecd90d
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2024-10-16 07:42:33 +0530

    Admin Home and side bar added in admin Dashboard

--- src/Components/AdminPages/adminDashboard/adminDashboard.jsx
+++ src/Components/AdminPages/adminDashboard/adminDashboard.jsx
@@ -1,13 +1,38 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet,Link } from "react-router-dom";
+import { ListGroup, ListGroupItem} from 'reactstrap'
 
 function AdminDashboard() {
   return (
-    <div>
-      AdminDashboard... this will have the Left side menu with add,update,delete products
-      <Outlet />
+    <div className="col-sm-12 p-0 row m-0">
+      <div className="col-sm-3 p-0 mt-5">
+        <ListGroup className="text-center">
+          <ListGroupItem
+            className="py-3"
+            tag={Link}
+            to={"/admin-dashboard/home"}
+            action="true"
+          >
+            <h5 className="admin-nav-font"> Home</h5>
+          </ListGroupItem>
+
+          <ListGroupItem
+            className="py-3"
+            tag={Link}
+            to={"/admin-dashboard/add-product"}
+            action="true"
+          >
+            <h5 className="admin-nav-font">Add Product</h5>
+          </ListGroupItem>
+        </ListGroup>
+      </div>
+      <div className="col-sm-9 p-0 text-center ">
+        <Outlet />
+      </div>
     </div>
   );
 }
 
 export default AdminDashboard;
+
+
