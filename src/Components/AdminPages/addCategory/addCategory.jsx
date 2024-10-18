949dafa95179bc57f18158d211f43f118e8dc5e3
RUPESH KUMBHAR <rupesh.p.kumbhar@gmail.com>
2024-10-18 07:10:31 +0530

    Add Product, Upload Product Image, Add Category

--- src/Components/AdminPages/addCategory/addCategory.jsx
+++ src/Components/AdminPages/addCategory/addCategory.jsx
@@ -1,9 +1,30 @@
 import React from "react";
+import "../addCategory/addCategory.scss"
+import { Container,Card, CardBody, CardText, Label,Input, Button} from "reactstrap";
+
 
 function AddCategory(){
-    return(
-        <div className="col-sm-12 p-0">addCategory</div>
-    )
+    return (
+      <div className="col-sm-12 p-0 mt-5">
+        <Container>
+          <Card color="light">
+            <CardBody>
+              <CardText className="text-center">
+                <label><b><h1>Add Category</h1></b></label>
+              </CardText>
+
+              <CardText>
+                <label><b>Category Title </b></label>
+                <Input className="mt-3" type="text"></Input>
+              </CardText>
+              <div className="col-sm-12 p-0 text-center">
+                <Button className="mt-4 create-cat w-25">Create</Button>
+              </div>
+            </CardBody>
+          </Card>
+        </Container>
+      </div>
+    );
 }
 
 export default AddCategory
